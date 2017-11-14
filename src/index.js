aba73b0045d6588a12b92432f1a02ba6df943c7d
Khalid <khalid@alum.recurse.com>
2017-11-14 19:45:25 +0000

    Build and test Cell component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+
+function Cell(props) {
+    let className = "cell";
+    if (props.isAlive) {
+        className = className.concat(" alive")
+    }
+    return (
+        <div className={ className } onClick= { props.onClick }>
+        </div>
+    )
+};
+
+
+ReactDOM.render(
+<Cell isAlive="true" />,
+document.getElementById("root")
+);
