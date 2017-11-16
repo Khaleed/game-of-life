80120c5fd6183a68a9e80e1df57618a9c978c85c
Khalid <khalid@alum.recurse.com>
2017-11-16 19:34:51 +0000

    Set up and test initial basic components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { map, reduce, filter } from "./helpers";
+import { range } from "./helpers";
 
 function Cell(props) {
-    let className = "cell";
-    if (props.isAlive) {
-        className = className.concat(" alive")
-    }
+    const className = "cell";
     return (
-        <div className={ className } onClick= { props.onClick }>
+        <div className={ props.isAlive? className.concat(" alive") : className }
+             onClick= { props.onClick }>
         </div>
     )
 };
 
+function Grid(props) {
+    return(
+        <div className="grid">
+            { props.value }
+        </div>
+    )
+};
+
+// all state will be held in this top-level component
+function Game() {
+    const generations = 0; // initial seed
+    return (
+        <div>
+            <h1>Conway's Game of Life</h1>
+            <Grid />
+            <h2>Generation { generations } </h2>
+        </div>
+    )
+}
 
 ReactDOM.render(
-<Cell isAlive="true" />,
-document.getElementById("root")
+    <Game value="hello"/>,
+    document.getElementById("root")
 );
+
+
