7393a44829c314a8d3057d523191ca14ab69a2b9
Khalid <khalid@alum.recurse.com>
2017-11-19 11:21:03 +0000

    Start populating state in Game component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import { map, reduce, filter } from "./helpers";
 import { range } from "./helpers";
 
+// stateless child component 2
 function Cell(props) {
     const className = "cell";
     return (
@@ -13,25 +14,35 @@ function Cell(props) {
     )
 };
 
+// stateless child component 1
 function Grid(props) {
     return(
         <div className="grid">
-            { props.value }
         </div>
     )
 };
 
 // all state will be held in this top-level component
 function Game() {
-    const generations = 0; // initial seed
+    const generations = 0; // initial seed to create generations
+    const rows = range(10);
+    const cols = 10;
+    // refactor to take cols as arg instead of 10
+    const filler = () => Array(10).fill("null");
+    const matrix = map(filler);
+    const initialGrid = matrix(rows);
     return (
-        <div>
+        <div className="game-of-life">
             <h1>Conway's Game of Life</h1>
-            <Grid />
+            <Grid
+                rows = { rows.length }
+                cols = { cols }
+                initialGrid = { initialGrid }
+            />
             <h2>Generation { generations } </h2>
         </div>
     )
-}
+};
 
 ReactDOM.render(
     <Game value="hello"/>,
