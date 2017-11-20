1e476fd8e00bfbbf90ca27a4311d055a207d27f7
Khalid <khalid@alum.recurse.com>
2017-11-20 17:56:57 +0000

    Trying TDD by taking baby steps

---
+++ src/tests/game-logic.js
@@ -0,0 +1,44 @@
+import { range, map, reduce, filter, partial, compose } from "../helpers";
+
+/*  process the entire cells based on the four rules:
+    1) Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+    2) Any live cell with two or three live neighbours lives on to the next generation.
+    3) Any live cell with more than three live neighbours dies, as if by overpopulation.
+    4) Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+*/
+
+// matrix :: ([], Int) -> Matrix
+const matrix = (m, n) => {
+    const rows = range(3);
+    const innerArray = () => Array(n).fill(0);
+    const grid = map(innerArray);
+    return grid(rows);
+};
+
+// isAlive :: (Matrix) -> Bool
+const isAlive = (grid, m, n) => grid[m, n] ? 1 : 0;
+
+// colsInRow :: (Matrix) -> []
+const colsInRow = (grid, m) => grid[m];
+
+// horizontal :: (Int, Int) -> Matrix
+const horizontal = (x, y) => {
+    return [[x, y - 1], [x, y + 1]];
+};
+
+// vertical :: (Matrix) -> Matrix
+const vertical = (x, y) => {
+    return [[x - 1, y], [x + 1, y]];
+};
+
+// NorthWestDiagonal :: (Matrix) -> Matrix
+const NorthWestDiagonal = (x, y) => {
+    return [[x - 1, y - 1], [x + 1, y + 1]];
+};
+
+// NorthEastDiagonal :: (Matrix) -> Matrix
+const NorthEastDiagonal = (x, y) => {
+    return [[x - 1, y + 1], [x + 1, y - 1]];
+};
+
+export { matrix, isAlive, colsInRow, horizontal, vertical, NorthWestDiagonal, NorthEastDiagonal }
---
+++ src/tests/game-logic.test.js
@@ -0,0 +1,43 @@
+import { matrix, isAlive, colsInRow } from "./game-logic";
+import { horizontal, vertical, NorthWestDiagonal, NorthEastDiagonal } from "./game-logic.js";
+import { range, map, reduce, filter, compose } from "../helpers";
+
+test('initialise a grid with default dead cells', () => {
+    expect(matrix(3, 3)).toEqual([
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]]);
+});
+
+test('check status of a cell', () => {
+    expect(isAlive(matrix, 3, 3)).toBeLessThan(2);
+});
+
+test('get a row', () => {
+    const grid = matrix(3, 3);
+    expect(colsInRow(grid, 1)).toEqual([0, 0, 0]);
+});
+
+test('is cell within grid', () => {
+
+});
+
+test('get horizontal neighbours', () => {
+    // given cell is at (1, 1) in a 3 x 3 grid
+    expect(horizontal(1, 1)).toEqual([[1, 0], [1, 2]]);
+});
+
+test('get vertical neighbours', () => {
+    // given cell is at (1, 1) in a 3 x 3 grid
+    expect(vertical(1, 1)).toEqual([[0, 1], [2, 1]]);
+});
+
+test('get top-left to bottom-right diagonal neighbours', () => {
+    // given cell is at (1, 1) in a 3 x 3 grid
+    expect(NorthWestDiagonal(1, 1)).toEqual([[0, 0], [2, 2]]);
+});
+
+test('get top-right to bottom-left diagonal neighbours', () => {
+    // given cell is at (1, 1) in a 3 x 3 grid
+    expect(NorthEastDiagonal(1, 1)).toEqual([[0, 2], [2, 0]]);
+});
