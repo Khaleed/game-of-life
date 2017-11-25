6e02555de330ade8493570f425d765ca08c64034
Khalid <khalid@alum.recurse.com>
2017-11-25 22:34:50 +0000

    Add further tests for the functional core of the game

--- src/tests/game-logic.test.js
+++ src/tests/game-logic.test.js
@@ -1,44 +1,102 @@
-import { matrix, isAlive, colsInRow } from "./game-logic";
-import { horizontal, vertical, NorthWestDiagonal, NorthEastDiagonal } from "./game-logic.js";
-import { range, map, reduce, filter, compose } from "../helpers";
+import { matrix, isAlive, reproduce, isInside } from "./game-logic";
+import { horizontalPoints , verticalPoints } from "./game-logic";
+import { northWestDiagonalPoints, northEastDiagonalPoints } from "./game-logic";
+import { neighbourhoodPoints, aliveNeighbours, lessThanTwoNeighbours } from "./game-logic";
+import { twoOrThreeNeighbours, moreThanThreeNeighbours, threeNeighbours } from "./game-logic";
+import { nextGeneration } from "./game-logic"; // to be implemented
+import { map, reduce, filter, compose } from "../helpers";
 
-test('initialise a grid with default dead cells', () => {
+test("check an imported function is defined", () => {
+    expect(matrix(3, 3)).toBeDefined();
+});
+
+test("initialise a grid with default dead cells", () => {
     expect(matrix(3, 3)).toEqual([
         [0,0,0],
         [0,0,0],
         [0,0,0]]);
 });
 
-test('check status of a cell', () => {
+test("check status of a cell", () => {
     const grid = matrix(3, 3);
     expect(isAlive(grid, 1, 1)).toBeLessThan(2);
 });
 
-test('get a row', () => {
+test("turn a dead cell into a live one", () => {
     const grid = matrix(3, 3);
-    expect(colsInRow(grid, 1)).toEqual([0, 0, 0]);
+    expect((reproduce(grid, 2, 2))).toEqual(1);
 });
 
-test('is cell within grid', () => {
-
-});
-
-test('get horizontal neighbours', () => {
+test("get horizontal points of neighbours", () => {
     // given cell is at (1, 1) in a 3 x 3 grid
-    expect(horizontal(1, 1)).toEqual([[1, 0], [1, 2]]);
+    expect(horizontalPoints(1, 1)).toEqual([[1, 0], [1, 2]]);
 });
 
-test('get vertical neighbours', () => {
+test("get vertical points of neighbours", () => {
     // given cell is at (1, 1) in a 3 x 3 grid
-    expect(vertical(1, 1)).toEqual([[0, 1], [2, 1]]);
+    expect(verticalPoints(1, 1)).toEqual([[0, 1], [2, 1]]);
 });
 
-test('get top-left to bottom-right diagonal neighbours', () => {
+test("get top-left to bottom-right diagonal points of neighbours", () => {
     // given cell is at (1, 1) in a 3 x 3 grid
-    expect(NorthWestDiagonal(1, 1)).toEqual([[0, 0], [2, 2]]);
+    expect(northWestDiagonalPoints(1, 1)).toEqual([[0, 0], [2, 2]]);
 });
 
-test('get top-right to bottom-left diagonal neighbours', () => {
+test("get top-right to bottom-left diagonal points of neighbours", () => {
     // given cell is at (1, 1) in a 3 x 3 grid
-    expect(NorthEastDiagonal(1, 1)).toEqual([[0, 2], [2, 0]]);
+    expect(northEastDiagonalPoints(1, 1)).toEqual([[0, 2], [2, 0]]);
+});
+
+test("turn a dead cell into a live one", () => {
+    const grid = matrix(3, 3);
+    expect((reproduce(grid, 2, 2))).toEqual(1);
+});
+
+test("check if cell points are inside the grid", () => {
+    expect((isInside(3, 3, 2, 2))).toBeTruthy();
+});
+
+test("get coordinates of all points of neighbours", () => {
+    expect((neighbourhoodPoints(1, 1))).toEqual([
+        [1, 0], [1, 2], [0, 1], [2, 1], [0, 0], [2, 2], [0, 2], [2, 0]
+    ]);
+});
+
+test("check the number of neighbours that are alive", () => {
+    const grid = matrix(3, 3);
+    const newGrid = matrix(3, 3);
+    newGrid[1][2] = 1; // [[0, 0, 0], [0, 0, 1], [0, 0, 0]]
+    expect(aliveNeighbours(grid, 1, 1)).toEqual(0); // 0
+    expect(aliveNeighbours(newGrid, 1, 1)).toEqual(1); // 1
+});
+
+test("a live cell should die if less than two neighbours are alive", () => {
+    const grid = [[1, 0, 0], [0, 1, 0], [0, 0, 0]];
+    expect(lessThanTwoNeighbours(grid, 1, 1)).toEqual(0);
+});
+
+test("a live cell should live if it has two or three alive neighbours", () => {
+    const grid = [[1, 0, 1], [0, 1, 0], [0, 0, 0]];
+    expect(twoOrThreeNeighbours(grid, 1, 1)).toEqual(1);
+});
+
+test("a live cell should die if it has more than three live neighbours", () => {
+    const grid = [[1, 1, 1], [0, 1, 0], [1, 0, 0]];
+    expect(moreThanThreeNeighbours(grid, 1, 1)).toEqual(0);
+});
+
+test("a dead cell should become alive if it has exactly three neighbours", () => {
+    const grid = [[1, 1, 0], [0, 0, 0], [1, 0, 0]];
+    expect(threeNeighbours(grid, 1, 1)).toEqual(1);
+});
+
+test("check grid after a tick", () => {
+    const grid = [[1, 0, 1],
+                  [1, 1, 1],
+                  [0, 0, 1]];
+    expect(nextGeneration(grid)).toEqual([
+        [1, 0, 1],
+        [1, 0, 1],
+        [0, 0, 1]
+    ]);
 });
