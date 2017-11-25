3bd97856143ddf2153c724c70bbc81642ba921c2
Khalid <khalid@alum.recurse.com>
2017-11-25 22:23:08 +0000

    Fix types and add concat in pointfree style

--- src/helpers.js
+++ src/helpers.js
@@ -1,14 +1,16 @@
-// :: map (fn, xs) -> f
+// map :: (fn) -> (xs) -> fn
 const map = (f) => (array) => array.map(f);
-// :: filter (fn, xs) -> f
+// filter :: (fn) -> (xs) -> fn
 const filter = (f) => (array) => array.filter(f);
-// :: reduce (fn, xs) -> f
+// reduce :: (fn) -> (xs) -> fn
 const reduce = (f) => (array) => array.reduce(f);
-// :: range [x] -> [xs]
+// concat :: (x) -> (xs) -> fn
+const concat = (x) => (xs) => xs.concat(x);
+// range :: [x] -> [xs]
 const range = (x) => [...Array(x).keys()];
 // compose :: ([...fs]) -> fn
 const compose = (...fs) => fs.reduce((f, g) => (...args) => f(g(...args)));
 // partial :: (fn, [...xs]) -> fn
 const partial = (f, ...init) => (...rest) => f(...init, ...rest);
 
-export { map, reduce, filter, range, compose, partial };
+export { map, reduce, filter, concat, range, compose, partial };
