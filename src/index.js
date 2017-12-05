7604f694181589d9cf356bca87b418ab8c18768d
Khalid <khalid@alum.recurse.com>
2017-12-05 15:38:33 +0000

    Refactor components to render a 3 x 3 grid

--- src/index.js
+++ src/index.js
@@ -1,67 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { map, reduce, filter, concat } from "./helpers";
-import { matrix } from "../src/tests/game-logic";
+import { matrix } from "./tests/model.js";
 
-// stateless child component 2
 function Cell(props) {
-    const cellSelection = () => props.cellSelection(props.row, props.col);
-    return(
-        <div className= { props.cellClass }
-             id = { props.coordinate }
-             onClick = { cellSelection }
-        >
-        </div>
-    );
-};
+  return <div className={props.cellClass} coordinate={props.coordinate} />;
+}
 
-// stateless child component 1
 function Grid(props) {
-    const cells = [];
-    props.grid.map((_, m) => props.grid[m].map((_, n) => {
-        const cellClass = "cell";
-        cells.concat(
-            <Cell
-            cellClass = { props.grid[m][n] ? cellClass.concat(" alive") : cellClass }
-            coordinate = { [m, n] }
-            row = { m }
-            col = { n }
-            cellSelection = { props.cellSelection }
-            />
-        );
-    }));
+  const cells = [];
+  for (let m = 0; m < props.grid.length; m += 1) {
+    for (let n = 0; n < props.grid[m].length; n += 1) {
+      const className = props.grid[m][n] ? "cell alive" : "cell";
+      cells.push(
+        <Cell
+          cellClass={className}
+          coordinate={[m, n]}
+          key={[m, n]}
+          row={m}
+          col={n}
+        />
+      );
+    }
+  }
+  return <div className="grid"> {cells}</div>;
+}
 
-    return(
-        <div className="grid">
-        { cells }
-        </div>
-    );
-};
-
-// all state will be held in this top-level component and propagate to child components
 function Game() {
-    const generation = 0;
-    const rows = 3;
-    const cols = 3;
-    const initialGrid = matrix(rows, cols);
-    return (
-        <div className="game-of-life">
-            <h1>Conway's Game of Life</h1>
-            <Grid
-                rows = { rows }
-                cols = { cols }
-                grid = { initialGrid }
-                cellSelection = { cellSelection }
-            />
-            <h2>Generations { generation } </h2>
-        </div>
-    );
-};
+  const initialGrid = matrix(3, 3); //=> [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+  return (
+    <div className="game-of-life">
+      <h1>Conway's Game of Life</h1>
+      <Grid grid={initialGrid} />
+    </div>
+  );
+}
 
-ReactDOM.render(
-    <Game value="hello"/>,
-    document.getElementById("root")
-);
-
-
+ReactDOM.render(<Game />, document.getElementById("root"));
