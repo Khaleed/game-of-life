0cec3ff2a3af68d023264eb56ef03edb292723fa
Khalid <khalid@alum.recurse.com>
2017-12-15 14:04:39 +0000

    Change default file extension to .jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,61 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { Grid } from "./components/grid";
+import { Play } from "./components/play";
 import { matrix } from "./model";
 import { nextGridGeneration } from "./update";
-
-function Play(props) {
-  return (
-    <div className="play-btn">
-      <button type="button" onClick={() => props.generations()}>
-        Play
-      </button>
-    </div>
-  );
-}
-
-function Cell(props) {
-  return (
-    <div
-      className={props.cellClass}
-      coordinate={props.coordinate}
-      onClick={() => props.seed(props.row, props.col)}
-    />
-  );
-}
-
-function Grid(props) {
-  return <div className="flex-grid"> {cellsInGrid(props.grid)}</div>;
-
-  // TODO refactor
-  function cellsInGrid(grid) {
-    const matrix = [];
-    for (let m = 0; m < grid.length; m += 1) {
-      const cells = [];
-      let n;
-      for (n = 0; n < grid[m].length; n += 1) {
-        const className = grid[m][n] ? "cell alive" : "cell";
-        const id = `cell-${m}-${n}`; // key props is a hint at which child elements may be stable across different renders
-        cells.push(
-          <Cell
-            cellClass={className}
-            coordinate={[m, n]}
-            key={id}
-            row={m}
-            col={n}
-            seed={props.seed}
-          />
-        );
-      }
-      matrix.push(
-        <div className="row" key={`row-${m}`}>
-          {cells}
-        </div>
-      );
-    }
-    return matrix;
-  }
-}
 
 class Game extends React.PureComponent {
   constructor(props) {
