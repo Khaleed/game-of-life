b4a8679f7dfa9841655b17aaa926ba33af4f5fe2
Khalid <khalid@alum.recurse.com>
2017-12-18 23:31:09 +0000

    Tidy up helpers and include randomBetween

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,16 +1,19 @@
 // map :: (fn) -> (xs) -> fn
-const map = (f) => (array) => array.map(f);
+const map = f => array => array.map(f);
 // filter :: (fn) -> (xs) -> fn
-const filter = (f) => (array) => array.filter(f);
+const filter = f => array => array.filter(f);
 // reduce :: (fn) -> (xs) -> fn
-const reduce = (f) => (array) => array.reduce(f);
+const reduce = f => array => array.reduce(f);
 // concat :: (x) -> (xs) -> fn
-const concat = (x) => (xs) => xs.concat(x);
+const concat = x => xs => xs.concat(x);
 // range :: [x] -> [xs]
-const range = (x) => [...Array(x).keys()];
+const range = x => [...Array(x).keys()];
 // compose :: ([...fs]) -> fn
 const compose = (...fs) => fs.reduce((f, g) => (...args) => f(g(...args)));
 // partial :: (fn, [...xs]) -> fn
 const partial = (f, ...init) => (...rest) => f(...init, ...rest);
 
-export { map, reduce, filter, concat, range, compose, partial };
+// randomBetween :: (Int, Int) -> Int
+const randomBetween = (a, b) => Math.floor(Math.random() * b) + a;
+
+export { map, reduce, filter, concat, range, compose, partial, randomBetween };
