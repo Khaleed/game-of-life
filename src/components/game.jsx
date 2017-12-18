5a61c577610ac6030dc6d87ac63007c2630468e7
Khalid <khalid@alum.recurse.com>
2017-12-18 23:29:01 +0000

    Add Stop, Clear, and Randomise to Controls

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "../index.css";
 import { Grid } from "./grid";
-import { Play } from "./play";
+import { Play, Stop, Clear, Randomise } from "./controls";
 import { matrix } from "../model/model";
 import { nextGridGeneration } from "../update/update";
+import { randomBetween } from "../helpers/helpers";
 
 class Game extends React.PureComponent {
   constructor(props) {
@@ -26,25 +27,50 @@ class Game extends React.PureComponent {
     this.setState({
       initialGrid: newGrid
     });
-    this.tick();
+    this.intervalID = requestAnimationFrame(this.generations);
   };
 
-  tick = () => {
-    const requestAnimationFrame = window.requestAnimationFrame;
-    requestAnimationFrame(this.generations);
+  stop = () => {
+    cancelAnimationFrame(this.intervalID);
+  };
+
+  clear = () => {
+    this.setState({
+      initialGrid: matrix(25, 25)
+    });
+  };
+
+  randomise = () => {
+    const newGrid = [...this.state.initialGrid];
+    return newGrid.map((_, m) =>
+      newGrid[m].map((_, n) => {
+        // random number between 1 and 3
+        if (randomBetween(1, 3) === 1) {
+          newGrid[m][n] = 1;
+        } else {
+          newGrid[m][n] = this.state.initialGrid[m][n];
+        }
+        return this.setState({
+          initialGrid: newGrid
+        });
+      })
+    );
   };
 
   /*Returns a tree of React elements at a point in time.
-     On the next update to props or state, it returns a
-     different tree of React elements. React then figures out how
-     to efficiently update the UI using a diffing algorithm based on heuristics.
-   */
+       On the next update to props or state, it returns a
+       different tree of React elements. React then figures out how
+       to efficiently update the UI using a diffing algorithm based on heuristics.
+     */
   render() {
     return (
       <div className="life">
         <h1>Game of Life</h1>
         <Grid grid={this.state.initialGrid} seed={this.seed} />
         <Play generations={this.generations} />
+        <Stop stop={this.stop} />
+        <Clear clear={this.clear} />
+        <Randomise random={this.randomise} />
       </div>
     );
   }
