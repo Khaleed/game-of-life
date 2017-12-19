cedc7134a46361e423246e5d8575c66c6bd95764
Khalid <khalid@alum.recurse.com>
2017-12-19 20:52:19 +0000

    Create variable to make model more readable

--- src/model/model.js
+++ src/model/model.js
@@ -2,8 +2,9 @@ import { range, map } from "../helpers/helpers";
 
 // matrix :: ([], Int) -> Matrix
 export const matrix = (m, n) => {
+  const dead = 0;
   const rows = range(m);
-  const cols = () => Array(n).fill(0);
+  const cols = () => Array(n).fill(dead);
   const grid = map(cols);
   return grid(rows);
 };
